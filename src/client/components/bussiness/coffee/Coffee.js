ce45d78b61ae01056b09e11687c5cee9096928bd
onur.varsak <onur.varsak@obss.com.tr>
2021-05-01 16:12:22 +0300

    create Coffee component with css properties

--- src/client/components/bussiness/coffee/Coffee.js
+++ src/client/components/bussiness/coffee/Coffee.js
@@ -1,9 +1,53 @@
 import React from 'react';
+import PropTypes from 'prop-types'
+import '../../../styles/Coffee.css';
 
-function Coffee() {
+
+function Coffee({
+    category,
+    description,
+    id,
+    ingredients,
+    title
+}) {
+
+    const getIngreditionWithFormat = () => {
+        if (ingredients && ingredients.length > 0) {
+            let out = ingredients[0];
+
+            for(let i = 1; i < ingredients.length; i++) {
+                out += `, ${ingredients[i]}`;
+            }
+
+            return out;
+        }
+
+        return '';
+    }
+
+
   return (
-    <div>Coffee Component</div>
+    <div className="Container">
+        <div className="Title">{title}</div>
+        <div className="Description">{description}</div>
+        <div className="Ingredients">Ingreditions: {getIngreditionWithFormat()}</div>
+    </div>
   );
 }
 
+Coffee.propTypes = {
+    category: PropTypes.string,
+    description: PropTypes.string,
+    id: PropTypes.number,
+    ingredients: PropTypes.array,
+    title: PropTypes.string
+}
+
+Coffee.defaultProps = {
+    category: '',
+    description: '',
+    id: -1,
+    ingredients: [],
+    title: '',
+};
 export default Coffee;
