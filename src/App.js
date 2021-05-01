bff1c09cddfbfaccf00ea3ba5a7c58cef7b19df0
onur.varsak <onur.varsak@obss.com.tr>
2021-05-01 23:58:49 +0300

    finished left side category

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React, { useEffect, useState } from 'react';
 import Coffee from './client/components/bussiness/coffee/Coffee'
 import { getCoffees } from './server/CoffeeWs';
 import './App.css';
+import LeftSide from './client/components/bussiness/LeftSide/LeftSide';
 
 function App() {
   const [allCoffeesData, setAllCoffeesData] = useState([])
+  const [filteredCoffees, setFilteredCoffees] = useState([])
   const [coffeeCategories, setCoffeeCategories] = useState([])
+  const [searchKey, setSearchKey] = useState('');
+  const [currentSelectedCategoryName, setCurrentSelectedCategoryName] = useState('');
 
   useEffect(() => {
     getCoffees().then((res) => {
@@ -16,17 +20,71 @@ function App() {
         res.forEach(coffee => categories.add(coffee.category))
       }
       setAllCoffeesData(res);
-      setCoffeeCategories([...categories]);
+      setFilteredCoffees(res);
+      
+      const tempCategoriesArr = [...categories];
+      const allCategoriesObject = tempCategoriesArr.map((category, index) => {
+        return {
+          id: index,
+          name: category,
+          isSelect: false,
+        }
+      })
+      setCoffeeCategories(allCategoriesObject);
     })
   }, [])
 
+  useEffect(() => {
+     function changeFilteredCoffees(key) {
+      let filteredForCategory = [];
+  
+      if(currentSelectedCategoryName.toLowerCase() === 'all coffees' || currentSelectedCategoryName.toLowerCase() === '') {
+        filteredForCategory = [...allCoffeesData];
+      } else {
+        filteredForCategory = allCoffeesData.filter(coffee => coffee.category.toLowerCase() === currentSelectedCategoryName.toLowerCase())
+      }
+  
+  
+      if (key === '') {
+        setFilteredCoffees([...filteredForCategory]);
+      } else {
+        setFilteredCoffees([...filteredForCategory.filter(coffee => coffee.title.toLowerCase().includes(key.toLowerCase()))]);
+      }
+    }
+
+    changeFilteredCoffees(searchKey);
+  }, [searchKey, currentSelectedCategoryName, allCoffeesData])
+
+  const searchInputOnChange = (event) => {
+    setSearchKey(event.target.value);
+  }
+
+  const onChangeSelectCategory= (categoryName) => {
+    coffeeCategories.forEach(category => {
+      if(category.name.toLowerCase() === categoryName.toLowerCase()) {
+        if(category.isSelect) {
+          category.isSelect = false;
+          setCurrentSelectedCategoryName('');
+        } else {
+          category.isSelect = true;
+          setCurrentSelectedCategoryName(categoryName);
+        }
+      } else {
+        category.isSelect = false;
+      }
+    })
+  }
+
   
 
- 
   return (
     <div>
+      <LeftSide categories={coffeeCategories}
+        searchKey={searchKey}
+        onChangeSearchKey={searchInputOnChange}
+        onChangeSelectCategory={onChangeSelectCategory} />
       {
-        allCoffeesData.map(coffee => {
+        filteredCoffees.map(coffee => {
           return <Coffee 
             title={coffee.title}
             description ={coffee.description}
@@ -44,4 +102,3 @@ function App() {
 }
 
 export default App;
-
